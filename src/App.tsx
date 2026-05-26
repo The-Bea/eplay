b04f992c397fc9c26e345d1cd1f750cce05e3cc8
Beatriz <beaa.devops@gmail.com>
2026-05-25 23:20:38 -0300

    efood- parte 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+import Home from './Pages/Home'
+import Perfil from './Pages/Perfil'
+import Footer from './Footer'
+import { store } from './store'
+import { Provider } from 'react-redux'
+import Cart from './Cart'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/perfil/:id" element={<Perfil />} />
+        </Routes>
+        <Footer />
+        <Cart />
+        <ToastContainer position="bottom-right" autoClose={3000} />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
