c1dbad4eb2d57e3f5b9a0048ae296661a739b0f7
Beatriz <beaa.devops@gmail.com>
2026-05-28 02:11:38 -0300

    resolvendo o problema do carrinho

--- src/store/reducers/cart.ts
+++ src/store/reducers/cart.ts
@@ -4,11 +4,13 @@ import { Prato } from '../../ProductList'
 type PratoState = {
   items: Prato[]
   isOpen: boolean
+  step: number
 }
 
 const initialState: PratoState = {
   items: [],
-  isOpen: false
+  isOpen: false,
+  step: 1
 }
 
 const cartSlice = createSlice({
@@ -16,24 +18,48 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     add: (state, action: PayloadAction<Prato>) => {
-      const prato = state.items.find((item) => item.id === action.payload.id)
+      const prato = state.items.find(
+        (item) => item.id === action.payload.id
+      )
+
       if (!prato) {
         state.items.push(action.payload)
       } else {
         alert('Este prato ja foi adicionado ao carrinho')
       }
     },
+
     remove: (state, action: PayloadAction<number>) => {
-      state.items = state.items.filter((item) => item.id !== action.payload)
+      state.items = state.items.filter(
+        (item) => item.id !== action.payload
+      )
     },
+
     open: (state) => {
       state.isOpen = true
     },
+
     close: (state) => {
       state.isOpen = false
+    },
+
+    nextStep: (state) => {
+      state.step += 1
+    },
+
+    prevStep: (state) => {
+      state.step -= 1
     }
   }
 })
 
-export const { add, remove, open, close } = cartSlice.actions
+export const {
+  add,
+  remove,
+  open,
+  close,
+  nextStep,
+  prevStep
+} = cartSlice.actions
+
 export default cartSlice.reducer
